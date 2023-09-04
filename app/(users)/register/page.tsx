45560665eb18d139e28f4db4fb107f1b123b32ab
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-04 02:25:49 +0200

    Creating account from frontend.

--- app/(users)/register/page.tsx
+++ app/(users)/register/page.tsx
@@ -1,10 +1,38 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+
+const registerUser = async (
+  e: React.FormEvent,
+  { login, password }: { login: string; password: string }
+) => {
+  e.preventDefault();
+  try {
+    const res = await fetch(`http://localhost:3000/api/v1/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ login, password }),
+    });
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const Register = () => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="min-h-screen flex justify-center">
       <div className="container flex justify-center items-start">
-        <form className="form my-8 py-8 flex flex-col gap-4 w-full max-w-2xl">
+        <form
+          onSubmit={(e) => registerUser(e, { login, password })}
+          className="form my-8 py-8 flex flex-col gap-4 w-full max-w-2xl"
+        >
           <label className="formLabel">
             Username:
             <br />
@@ -12,6 +40,8 @@ const Register = () => {
               type="text"
               className="formInput w-full mt-2"
               placeholder="John"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
             />
           </label>
           <label className="formLabel">
@@ -21,6 +51,8 @@ const Register = () => {
               type="password"
               className="formInput w-full mt-2"
               placeholder="somethingverysecure123!"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </label>
           {/* <label className="formLabel">
