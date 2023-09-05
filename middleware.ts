9936003003fe1ba0af45873609d12cc8b8713f62
Piotr Filipiak <kiddhkane@gmail.com>
2023-09-05 16:55:47 +0200

    Figured out redirection vs rewrite.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log("Using middleware.");
+  if (request.nextUrl.pathname.startsWith("/about")) {
+    console.log("Going to about");
+  }
+
+  if (request.nextUrl.pathname.startsWith("/secrets")) {
+    console.log("Going to secrets");
+    return NextResponse.redirect(new URL("/about", request.url));
+    // return NextResponse.rewrite(new URL("/about", request.url));
+    // Rewrite still shows /secets path, redirect doesn't.
+  }
+
+  // console.log("Using middleware.");
   return NextResponse.next();
 }
